1f35efc54e020c5703fab256f5dc409644efcfb5
d4mk0 <d4mk0f@gmail.com>
2016-04-05 19:04:17 +0300

    Сущность заметок была добавлена

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405134652) do
+ActiveRecord::Schema.define(version: 20160405154942) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title",       null: false
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20160405134652) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "notes", force: :cascade do |t|
+    t.text     "text",       null: false
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
